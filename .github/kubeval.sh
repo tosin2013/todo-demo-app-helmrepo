3d8a2dd30c40c085848275b00e3875fa239a1d9e
Tosin Akinosho <takinosh@redhat.com>
2023-03-27 21:48:07 -0400

    adding kubeval

---
+++ .github/kubeval.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -exuo pipefail
+
+ls -R .
+
+
+CHART_DIRS="./charts/quarkuscoffeeshop-charts"
+KUBEVAL_VERSION="0.16.0"
+SCHEMA_LOCATION="https://raw.githubusercontent.com/instrumenta/kubernetes-json-schema/master/"
+REPO_PATH="gh-pages"
+
+# install kubeval
+curl --silent --show-error --fail --location --output /tmp/kubeval.tar.gz https://github.com/instrumenta/kubeval/releases/download/"${KUBEVAL_VERSION}"/kubeval-linux-amd64.tar.gz
+tar -xf /tmp/kubeval.tar.gz kubeval
+
+# validate charts
+for CHART_DIR in ${CHART_DIRS}; do
+  echo "Displaying Current path"
+  pwd
+  helm template --values "${CHART_DIR}"/values.yaml "${CHART_DIR}" | ./kubeval --strict --ignore-missing-schemas --kubernetes-version "${KUBERNETES_VERSION#v}" --schema-location "${SCHEMA_LOCATION}"
+done
