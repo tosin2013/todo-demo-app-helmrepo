8c0a392d5fe1f4083c04656826062c8f3fe76dc2
Tosin Akinosho <takinosh@redhat.com>
2023-06-21 10:58:06 -0400

    updating default repo

--- generate_vars.sh
+++ generate_vars.sh
@@ -7,10 +7,7 @@ ocp4_workload_gitea_operator_create_users: true
 ocp4_workload_gitea_operator_migrate_repositories: true
 ocp4_workload_gitea_operator_gitea_image_tag: 1.19.3
 ocp4_workload_gitea_operator_repositories_list:
-- repo: "https://github.com/tosin2013/rhacm-configuration"
-  name: "rhacm-configuration"
-  private: false
-- repo: "https://github.com/tosin2013/applications"
-  name: "applications"
+- repo: "https://github.com/tosin2013/todo-demo-app-helmrepo.git"
+  name: "todo-demo-app-helmrepo"
   private: false
 EOF
