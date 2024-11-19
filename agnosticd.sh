1a84b991a4d91f45d3ce47304c83e93ffc47626d
Tosin Akinosho <takinosh@redhat.com>
2024-11-19 13:47:15 -0500

    Update agnosticd.sh

--- agnosticd.sh
+++ agnosticd.sh
@@ -62,32 +62,58 @@ if [ -d "$HOME/agnosticd" ]; then
   git checkout ${agnosticd_workload}
   git pull
 else
+  cd $HOME
   git clone $agnosticd_repo
   cd $HOME/agnosticd
 fi
 
+# 1. Copies the kubeconfig file from the specified source directory to the home directory of the current user.
+# 2. Creates the .kube directory in the user's home directory if it does not already exist.
+# 3. Changes the ownership of the copied kubeconfig file to the current user and the 'users' group.
+# 4. Sets the KUBECONFIG environment variable to point to the copied kubeconfig file.
 sudo cp /home/ec2-user/cluster-$GUID/auth/kubeconfig /home/${USER}
 mkdir -p . /home/${USER}/.kube/
 sudo chown ${USER}:users /home/${USER}/kubeconfig
 export KUBECONFIG=/home/${USER}/kubeconfig
 
 
+# This script checks if the user is logged into OpenShift.
+# If the user is not logged in, it prints a message instructing the user to log in using 'oc login'
+# and exits with a status code of 1.
 if ! oc whoami &> /dev/null; then
   echo "You are not logged into OpenShift. Please log in using 'oc login'."
   exit 1
 fi
 
+# This script performs the following actions:
+# 1. Downloads the todo-demo-app-helmrepo.yaml file from the specified GitHub repository URL and saves it to ansible/configs directory.
+# 2. Updates the 'cluster_one.api_key' field in the downloaded YAML file with the value of the OPENSHIFT_API_KEY environment variable.
+# 3. Updates the 'cluster_one.api_url' field in the downloaded YAML file with the value of the OPENSHIFT_API_URL environment variable.
+# 4. Updates the 'guid' field in the downloaded YAML file with the value of the guid environment variable.
+# If any of the yq commands fail, the script exits with the corresponding error code.
 curl -L https://raw.githubusercontent.com/tosin2013/rhel9-bootstrap/refs/heads/main/agnosticd_templates/ocp-workloads/todo-demo-app-helmrepo.yaml -o ansible/configs/todo-demo-app-helmrepo.yaml
 yq e '.cluster_one.api_key = "'${OPENSHIFT_API_KEY}'"' -i "/home/${USER}/agnosticd/ansible/configs/todo-demo-app-helmrepo.yaml" || exit $?
 yq e '.cluster_one.api_url = "'${OPENSHIFT_API_URL}'"' -i "/home/${USER}/agnosticd/ansible/configs/todo-demo-app-helmrepo.yaml" || exit $?
 yq e '.guid = "'${guid}'"' -i "/home/${USER}/agnosticd/ansible/configs/todo-demo-app-helmrepo.yaml" || exit $?
 
+
+# This script runs an Ansible playbook using ansible-navigator.
+# The playbook to be executed is located at ansible/main.yml.
+# The execution environment image (EEI) used is from quay.io/agnosticd/ee-multicloud:latest.
+# The KUBECONFIG environment variable is passed to the execution environment.
+# Additional variables are loaded from ansible/configs/todo-demo-app-helmrepo.yaml.
+# The output mode is set to stdout, and verbose mode is enabled.
 ansible-navigator run ansible/main.yml \
           --eei quay.io/agnosticd/ee-multicloud:latest \
           --pass-environment-variable KUBECONFIG \
-          -e @ansible/configs/todo-demo-app-helmrepo.yaml  -m stdout -vvv
+          -e @ansible/configs/todo-demo-app-helmrepo.yaml  -m stdout -v
 
 
-ansible-navigator run ansible/destroy.yml \
-      --eei quay.io/agnosticd/ee-multicloud:latest \
-      -e @ansible/configs/todo-demo-app-helmrepo.yaml   -m stdout
+# This script is intended to run an Ansible playbook to destroy resources.
+# It uses the ansible-navigator tool to execute the playbook located at ansible/destroy.yml.
+# The execution environment image (EEI) used is quay.io/agnosticd/ee-multicloud:latest.
+# The playbook is provided with extra variables from the file ansible/configs/todo-demo-app-helmrepo.yaml.
+# The output mode is set to stdout.
+#ansible-navigator run ansible/destroy.yml \
+#      --eei quay.io/agnosticd/ee-multicloud:latest \
+#      -e @ansible/configs/todo-demo-app-helmrepo.yaml   -m stdout
